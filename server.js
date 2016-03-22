f2dc0ceedc8afe86e8954594d085f195021bc793
Mariia Paniutina <mariia.paniutina@gmail.com>
2016-03-21 22:57:19 -0700

    test...

--- server.js
+++ server.js
@@ -1,3 +1,24 @@
+/*
 var connect = require('connect');
 var serveStatic = require('serve-static');
 connect().use(serveStatic(__dirname)).listen(8082);
+*/
+
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+//app.use(express.static(__dirname + '/build'));
+
+//adding that to have sourcemap enabled....
+//will rewrite that in future
+app.use(express.static(__dirname + '/')); 
+
+app.get('/', function(request, response) {
+  response.sendfile(__dirname + '/index.html');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
